d8944d63b734d410678eba73ed564502ac044fae
Charan Kanwal Singh <kanwalcharan74@gmail.com>
2026-08-27 18:22:09 +0530

    Fix deployment routing

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,30 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    path("",views.home,name="home"),
+    path("predict/", views.predict_risk,name="predict_risk"),
+    path("history/",views.history,name="history"),
+    path("clear-history/",views.clear_history,name="clear_history"),
 
-    path(
-        "",
-        views.home,
-        name="home"
-    ),
-
-    path(
-        "predict/",
-        views.predict_risk,
-        name="predict_risk"
-    ),
-
-    path(
-        "history/",
-        views.history,
-        name="history"
-    ),
-
-    path(
-        "clear-history/",
-        views.clear_history,
-        name="clear_history"
-    ),
 ]
